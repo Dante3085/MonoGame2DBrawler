a8e60bfb5ee6d0896dbf008d7075e573fa47b23f
mjsch <moritz97.s@web.de>
2018-07-25 23:57:36 +0200

    2DBrawler Projekt in Repo

---
+++ MonoGame2DBrawler/MonoGame2DBrawler/Input/GamePadInput.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame2DBrawler.Input
+{
+    /// <summary>
+    /// Maks very common Game inputs with their respective names (Left, Right, etc.).
+    /// </summary>
+    public class GamePadInput
+    {
+        public Buttons Left { get; set; }
+        public Buttons Up { get; set; }
+        public Buttons Right { get; set; }
+        public Buttons Down { get; set; }
+        public Buttons Attack { get; set; }
+    }
+}
---
+++ MonoGame2DBrawler/MonoGame2DBrawler/Input/KeyboardInput.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame2DBrawler.Input
+{
+    /// <summary>
+    /// Maks very common Game inputs with their respective names (Left, Right, etc.).
+    /// </summary>
+    public class KeyboardInput
+    {
+
+        public Keys Left { get; set; }
+        public Keys Up { get; set; }
+        public Keys Right { get; set; }
+        public Keys Down { get; set; }
+        public Keys Attack { get; set; }
+    }
+}
