6d8c7ebc6a1903f4164a8d5a15e287fbfa7ccbe0
mjsch <moritz97.s@web.de>
2018-07-26 22:29:12 +0200

    SHORT: DrawBoundingBox() deutlich effizienter (keine new Anweisungen mehr); DrawBoundingBox() Aufruf bei Kollision funktioniert;

--- MonoGame2DBrawler/MonoGame2DBrawler/Game1.cs
+++ MonoGame2DBrawler/MonoGame2DBrawler/Game1.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using VosSoft.Xna.GameConsole;
+
+using MonoGame2DBrawler.Sprites;
+using MonoGame2DBrawler.Input;
 
 namespace MonoGame2DBrawler
 {
@@ -12,10 +17,18 @@ namespace MonoGame2DBrawler
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        public static GameConsole gameConsole;
+
+        private List<AnimatedSprite> animSprites;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
 
         /// <summary>
@@ -27,6 +40,7 @@ namespace MonoGame2DBrawler
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            gameConsole = new GameConsole(this, "german", Content);
 
             base.Initialize();
         }
@@ -41,6 +55,22 @@ namespace MonoGame2DBrawler
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Texture2D playerSheet = Content.Load<Texture2D>("playerSheet");
+
+            animSprites = new List<AnimatedSprite>()
+            {
+                new AnimatedSprite("Player 1", new Vector2(100, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 2", new Vector2(200, 100), PlayerIndex.Two, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(300, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(400, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(500, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(600, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(700, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(800, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(900, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+                new AnimatedSprite("Player 3", new Vector2(1000, 100), PlayerIndex.One, GraphicsDevice, playerSheet),
+            };
+
         }
 
         /// <summary>
@@ -62,7 +92,24 @@ namespace MonoGame2DBrawler
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Tab))
+                gameConsole.Open(Keys.Tab);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.F1))
+            {
+                foreach (AnimatedSprite s in animSprites)
+                    s.PDrawBoundingBox = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.F2))
+            {
+                foreach (AnimatedSprite s in animSprites)
+                    s.PDrawBoundingBox = false;
+            }
+
             // TODO: Add your update logic here
+
+            foreach (AnimatedSprite s in animSprites)
+                s.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -76,6 +123,12 @@ namespace MonoGame2DBrawler
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            foreach (AnimatedSprite s in animSprites)
+                s.Draw(spriteBatch, animSprites);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
